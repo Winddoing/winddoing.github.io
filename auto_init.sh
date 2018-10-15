b6cf8b5a555840b51221326fd3af1bda45e7b2eb
winddoing <winddoing@sina.cn>
2018-10-15 10:23:15 +0800

    add site state

--- auto_init.sh
+++ auto_init.sh
@@ -10,10 +10,15 @@ echo "Current path: $PWD"
 echo "Current dirs:"
 ls
 
-node_modules_sz=`du -sh ./node_modules | awk '{print $1}'`
+node_modules_sz=`du -sh ./node_modules | awk '{print int($1)}'`
 all_sz=`du -sh  $SRC_DIR | awk '{print int($1)}'`
 sz=$(($all_sz-$node_modules_sz))
 
 echo "Data size: $sz"
-
 sed -i "s/xxx/$sz/g" `grep -l xxx source/about/index.md`
+
+
+
+xdate=`date +%Y.%m.%d-%H:%M:%S`
+echo "Current time: $xdate"
+sed -i "s/xdate/$xdate/g" `grep -l xdate source/about/index.md`
