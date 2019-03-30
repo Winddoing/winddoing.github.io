0b6099715224007209ec1967633b6ecd328c2704
winddoing <winddoing@sina.cn>
2019-03-31 02:58:38 +0800

    修改样式

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -122,15 +122,15 @@
                     var target = src + (type === 'video' ? '.mp4' : '.jpg');
                     src += '';
                     liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">\
-            <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
-              <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="assets/empty.png" itemprop="thumbnail" onload="lzld(this)">\
-            </a>\
-            <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
-        </figure>';
+                        <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
+                          <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="assets/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+                        </a>\
+                        <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
+                    </figure>';
                 }
                 ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '年<em>' + data.month + '月</em></h1>\
-    <ul class="img-box-ul">' + liTmpl + '</ul>\
-    </section>';
+                                      <ul class="img-box-ul">' + liTmpl + '</ul>\
+                                   </section>';
             }
             document.querySelector('.instagram').innerHTML = '<div class="photos" itemscope itemtype="http://schema.org/ImageGallery">' + ulTmpl + '</div>';
             createVideoIncon();
