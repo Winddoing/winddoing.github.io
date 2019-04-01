6625aba30f206835ef1ca0ffe6d4b7071dad71a9
shaowangquan <shaowangquan@vanxum.com>
2019-04-01 14:03:58 +0800

    修改data.json路径

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -168,7 +168,8 @@
         function loadData(success) {
             if (!searchData) {
                 var xhr = new XMLHttpRequest();
-                xhr.open('GET', './data.json?t=' + +new Date(), true);
+                /* xhr.open('GET', './data.json?t=' + +new Date(), true); */
+                xhr.open('GET', 'https://coding.net/u/Winddoing/p/photos/git/raw/master/data.json?t=' + +new Date(), true);
 
                 xhr.onload = function() {
                     if (this.status >= 200 && this.status < 300) {
