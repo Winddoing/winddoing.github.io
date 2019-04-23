11b3f852f5c1be1f4f0a1ef84f6e30670e4fb2e3
winddoing <winddoing@sina.cn>
2019-04-23 09:34:24 +0800

    fix site source size

--- auto_init.sh
+++ auto_init.sh
@@ -8,13 +8,15 @@ PWD=`pwd`
 
 echo "Current path: $PWD"
 echo "Current dirs:"
-ls
+
+ls -lha
 
 node_modules_sz=`du -sh ./node_modules | awk '{print int($1)}'`
+git_sz=`du -sh .git | awk '{print int($1)}'`
 all_sz=`du -sh  $SRC_DIR | awk '{print int($1)}'`
-sz=$(($all_sz-$node_modules_sz))
+sz=$(($all_sz-$node_modules_sz-$git_sz))
 
-echo "all size: $all_sz, node_modules size: $node_modules_sz"
+echo "all size: $all_sz, node_modules size: $node_modules_sz, .git size: $git_sz"
 echo "Data size: $sz"
 sed -i "s/xxx/$sz/g" `grep -l xxx source/about/index.md`
 
