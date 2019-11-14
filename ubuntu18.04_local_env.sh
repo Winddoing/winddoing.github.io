3f98f89d4a1f7fbe35b6994505fee42b1826efa3
winddoing <winddoing@sina.cn>
2019-11-14 11:41:45 +0800

    hexo 本地环境

---
+++ ubuntu18.04_local_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+##########################################################
+# File Name		: ubuntu18.04_local.sh
+# Author		: winddoing
+# Created Time	: 2019年11月14日 星期四 11时37分42秒
+# Description	:
+##########################################################
+
+sudo apt install nodejs-dev libssl1.0-dev npm
+
+npm install
